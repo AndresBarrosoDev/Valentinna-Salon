407323384582e9d65a78a64912ef73ddc2d32513
Andres Barroso <andres.barroso@gmail.com>
2021-12-08 19:29:06 -0300

    Third attempt to make gh-pages work

---
+++ precache-manifest.73d363d1eab4865995eb98b4aca9c485.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9724a961c2d22e86a7d8",
+    "url": "/css/app.dd611d03.css"
+  },
+  {
+    "revision": "22e2b17d0814040cb3b2",
+    "url": "/css/chunk-vendors.82cb7dcb.css"
+  },
+  {
+    "revision": "94499c86b5acaa219d220b19cfa03def",
+    "url": "/img/reservaCreada10.2.94499c86.png"
+  },
+  {
+    "revision": "3ccaff385b6ca73b8287231da285b13d",
+    "url": "/img/valentinna-salon.3ccaff38.png"
+  },
+  {
+    "revision": "32c59e96e9381ab496e59b9deb945613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9724a961c2d22e86a7d8",
+    "url": "/js/app.3a3992ee.js"
+  },
+  {
+    "revision": "22e2b17d0814040cb3b2",
+    "url": "/js/chunk-vendors.ecee5628.js"
+  },
+  {
+    "revision": "843d9b9d39ebeb71d55633d5cd80a3be",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
